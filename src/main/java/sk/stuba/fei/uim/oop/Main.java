0e39b2f5bc94f971654ff2d0a89e1932df5bb468
Jakub <xchrappaj@stuba.sk>
2022-03-23 14:36:19 +0100

    zaklad asi

--- src/main/java/sk/stuba/fei/uim/oop/Main.java
+++ src/main/java/sk/stuba/fei/uim/oop/Main.java
@@ -21,36 +21,13 @@ public class Main {
         JPanel panel = new JPanel();
         frame.add(panel);
 
-        placeComponents(panel);
+        Place place = new Place();
+
+        place.placeComponents(panel);
 
 
         frame.setVisible(true);
     }
 
-    private static void placeComponents(JPanel panel) {
 
-        panel.setLayout(null);
-
-        JLabel userLabel = new JLabel("User");
-
-        userLabel.setBounds(10,20,80,25);
-        panel.add(userLabel);
-
-        JTextField userText = new JTextField(20);
-        userText.setBounds(100,20,165,25);
-        panel.add(userText);
-
-
-        JLabel passwordLabel = new JLabel("Password");
-        passwordLabel.setBounds(10,50,80,25);
-        panel.add(passwordLabel);
-
-        JPasswordField passwordText = new JPasswordField(20);
-        passwordText.setBounds(100,50,165,25);
-        panel.add(passwordText);
-
-        JButton loginButton = new JButton("login");
-        loginButton.setBounds(10, 80, 80, 25);
-        panel.add(loginButton);
-    }
 }
---
+++ src/main/java/sk/stuba/fei/uim/oop/Place.java
@@ -0,0 +1,34 @@
+package sk.stuba.fei.uim.oop;
+
+import javax.swing.*;
+
+public class Place {
+
+    public void placeComponents(JPanel panel) {
+
+        panel.setLayout(null);
+
+        JLabel userLabel = new JLabel("User");
+
+        userLabel.setBounds(10,20,80,25);
+        panel.add(userLabel);
+
+        JTextField userText = new JTextField(20);
+        userText.setBounds(100,20,165,25);
+        panel.add(userText);
+
+
+        JLabel passwordLabel = new JLabel("Password");
+        passwordLabel.setBounds(10,50,80,25);
+        panel.add(passwordLabel);
+
+        JPasswordField passwordText = new JPasswordField(20);
+        passwordText.setBounds(100,50,165,25);
+        panel.add(passwordText);
+
+        JButton loginButton = new JButton("login");
+        loginButton.setBounds(10, 80, 80, 25);
+        panel.add(loginButton);
+    }
+
+}
